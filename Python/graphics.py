7f7a9eb520c5b18cffc9820d18fe43c3245ed502
Merly C M <merlymurali000@gmail.com>
2025-10-29 09:55:40 +0530

    python

--- Python/graphics.py
+++ Python/graphics.py
@@ -1,12 +1,12 @@
 import turtle
 
 t = turtle.Turtle()
-t.speed(-30000000)
+t.speed(0)
 t.pensize(2)
 turtle.bgcolor("navyblue")
 
 colors = ["red", "yellow"]
-for i in range(7600):
+for i in range(100):
     t.color(colors[i % 2]) 
     t.circle(100)
     t.right(10)
